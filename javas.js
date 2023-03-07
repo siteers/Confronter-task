66343fc3fd28f15b759d5c52b2fc3773dda63607
Krzysztof Sitko <122014903+siteers@users.noreply.github.com>
2023-03-07 11:13:07 +0100

    Add files via upload

---
+++ javas.js
@@ -0,0 +1,6 @@
+function showValue(newValue) {
+        document.getElementById("rangeValue").innerHTML = "<h4>Kwota <y style=color:white;>" + newValue +" zł</h4></y>";
+      }
+      function showValue2(newValue) {
+        document.getElementById("rangeValue2").innerHTML = "<h4>Okres <y style=color:white;> " + newValue +" dni</h4></y>";
+      }
